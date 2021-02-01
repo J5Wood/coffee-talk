0ed13ac7a6b7d3f6a2af1337b822397ea8da1df0
Jeremy Wood <js.wood206@gmail.com>
2021-02-01 12:27:14 -0800

    Created Coffee controller routes

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,2 +1,29 @@
 class CoffeesController < ApplicationController
+
+    def index
+        @coffees = Coffee.all
+    end
+
+    def new
+        @coffee = Coffee.new
+    end
+
+    def create
+        @coffee = Coffee.new
+    end
+
+    def edit
+        @coffee = Coffee.find_by(id: params[:id])
+    end
+
+    def update
+        @coffee = Coffee.find_by(id: params[:id])
+    end
+
+    def show
+        @coffee = Coffee.find_by(id: params[:id])
+    end
+
+    def delete
+    end
 end
