06ec5d5dcb6a6ea7ee3517d06d3e6ebe75e3566c
Jeremy Wood <js.wood206@gmail.com>
2021-02-01 13:11:15 -0800

    Created Coffees views pages, stubbed out coffees index

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -2,4 +2,18 @@ class Coffee < ApplicationRecord
     belongs_to :brand
     has_many :reviews
     has_many :users, through: :reviews
+
+    def roast_level
+        if roast == 1
+            "Light"
+        elsif roast == 2
+            "Medium Light"
+        elsif roast == 3
+            "Medium"
+        elsif roast == 4
+            "Medium Dark"
+        else
+            "Dark"
+        end
+    end
 end
