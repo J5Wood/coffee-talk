a33540d0b1320ac7cd98dea09529671e1c36acdd
Jeremy Wood <js.wood206@gmail.com>
2021-02-01 12:14:07 -0800

    Added user/session login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
 class SessionsController < ApplicationController
 
     def new
-        
+        @user = User.new
     end
 
     def create
-
+        user = User.where(name: params[:user][:name])[0]
+        if user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            @user = User.new
+            render :new
+        end
     end
 
     def delete
