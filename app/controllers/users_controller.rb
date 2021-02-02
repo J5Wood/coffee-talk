1f586159894e28fab4aa9055e8116e0a8a4c6a95
Jeremy Wood <js.wood206@gmail.com>
2021-02-02 09:32:06 -0800

    Created new user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,24 @@ class UsersController < ApplicationController
     def show
         @user = User.find_by(id: session[:user_id])
     end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if !User.find_by(name: params[:user][:name]) && @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password, :email)
+    end
 end
