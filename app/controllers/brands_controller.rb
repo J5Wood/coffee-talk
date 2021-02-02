7744d542d39a25605aaa443cb8107e336c9ec219
Jeremy Wood <js.wood206@gmail.com>
2021-02-01 21:34:54 -0800

    Created new and edit pages for Brand

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -4,7 +4,39 @@ class BrandsController < ApplicationController
         @brands = Brand.all
     end
 
+    def new
+        @brand = Brand.new
+    end
+
+    def create
+        @brand = Brand.new(brand_params)
+        if @brand.save
+            redirect_to brand_path(@brand)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @brand = Brand.find_by(id: params[:id])
+    end
+
+    def update
+        @brand = Brand.find_by(id: params[:id])
+        if @brand.update(brand_params)
+            redirect_to brand_path(@brand)
+        else
+            render :edit
+        end
+    end
+
     def show
         @brand = Brand.find_by(id: params[:id])
+    end
+
+    private
+
+    def brand_params
+        params.require(:brand).permit(:name, :location)
     end
 end
