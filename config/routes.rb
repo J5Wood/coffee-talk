2552e6fa1d3614cfd6f68681bb90dcd7d5d36cad
Jeremy Wood <js.wood206@gmail.com>
2021-02-02 10:27:01 -0800

    Moved User form to partial

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get "login" => "sessions#new"
   post "login" => "sessions#create"
-  post "logout" => "sessions#delete"
+  post "logout" => "sessions#destroy"
   
   resources :coffees do
     resources :reviews, only: [:new, :edit]
