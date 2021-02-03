94d240226f7b852cf9339c7894448ede50138a48
Jeremy Wood <js.wood206@gmail.com>
2021-02-03 12:19:45 -0800

    Added more validations to User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,15 @@
 class UsersController < ApplicationController
-
-    def show
-        @user = User.find_by(id: session[:user_id])
-    end
+    before_action :require_login
+    skip_before_action :require_login, only: [:new]
 
     def new
+        redirect_if_logged_in
         @user = User.new
     end
 
     def create
         @user = User.new(user_params)
-        if !User.find_by(name: params[:user][:name]) && @user.save
+        if @user.save
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
@@ -18,18 +17,21 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+        find_current_user
+    end
+
     def edit
-        @user = User.find_by(id: params[:id])
+        find_current_user
     end
 
     def update
-        @user = User.find_by(id: params[:id])
+        find_current_user
         if @user.update(user_params)
             redirect_to user_path(@user)
         else
             render :edit
         end
-
     end
 
     def destroy
@@ -42,5 +44,21 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:name, :password, :email, :password_confirmation)
+    end
+
+    def require_login
+        if !session[:user_id]
+            redirect_to '/'
+        end
+    end
+
+    def find_current_user
+        @user = User.find_by(id: session[:user_id])
+    end
+
+    def redirect_if_logged_in
+        if !!session[:user_id]
+            redirect_to '/'
+        end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     has_many :coffees, through: :reviews
     has_secure_password
     validates :name, :password, :email, presence: true
+    validates :name, uniqueness: true
 end
