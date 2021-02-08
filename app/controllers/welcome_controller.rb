be66c33c90d12e0dec54fbe6a6e6ab54868499af
Jeremy Wood <js.wood206@gmail.com>
2021-02-07 18:16:37 -0800

    Stubbed out home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
 
     def home
+        @coffees = Coffee.order(stars: :desc).limit(5)
+        @brands = Brand.top_rated
     end
 end
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -3,4 +3,21 @@ class Brand < ApplicationRecord
     has_many :reviews, through: :coffees
     validates :name, presence: true
     validates :name, uniqueness: true
+
+    def average_rating
+        if coffees.any?
+            star_array = []
+            coffees.each do |coffee|
+                star_array << coffee.stars
+            end
+            average = star_array.sum(0.0) / star_array.size
+            average = average.to_i
+        else
+            0
+        end
+    end
+
+    def self.top_rated
+        Brand.all.sort_by { |brand| brand.average_rating }[0,5].reverse() 
+    end
 end
