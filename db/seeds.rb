069b0a55a9af8e892d2a2e89b6bf7cc99da6ad91
Jeremy Wood <js.wood206@gmail.com>
2021-02-14 16:32:34 -0800

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,32 +30,32 @@ User.create(name: "Jill", password: "password!")
 User.create(name: "Ben", password: "password!")
 User.create(name: "Jen", password: "password!")
 
-f.reviews.create(content: "Review Number 1", user_id: 1)
-f.reviews.create(content: "Review Number 2", user_id: 3)
+f.reviews.create(content: "Review Number 1", user_id: 1, stars: 4)
+f.reviews.create(content: "Review Number 2", user_id: 3, stars: 3)
 
-g.reviews.create(content: "Review Number 3", user_id: 2)
-g.reviews.create(content: "Review Number 4", user_id: 4)
+g.reviews.create(content: "Review Number 3", user_id: 2, stars: 4)
+g.reviews.create(content: "Review Number 4", user_id: 4, stars: 2)
 
-h.reviews.create(content: "Review Number 5", user_id: 1)
-h.reviews.create(content: "Review Number 6", user_id: 2)
+h.reviews.create(content: "Review Number 5", user_id: 1, stars: 1)
+h.reviews.create(content: "Review Number 6", user_id: 2, stars: 4)
 
-i.reviews.create(content: "Review Number 7", user_id: 3)
-i.reviews.create(content: "Review Number 8", user_id: 4)
+i.reviews.create(content: "Review Number 7", user_id: 3, stars: 4)
+i.reviews.create(content: "Review Number 8", user_id: 4, stars: 4)
 
-j.reviews.create(content: "Review Number 9", user_id: 1)
-j.reviews.create(content: "Review Number 10", user_id: 4)
+j.reviews.create(content: "Review Number 9", user_id: 1, stars: 5)
+j.reviews.create(content: "Review Number 10", user_id: 4, stars: 1)
 
-k.reviews.create(content: "Review Number 11", user_id: 2)
-k.reviews.create(content: "Review Number 12", user_id: 3)
+k.reviews.create(content: "Review Number 11", user_id: 2, stars: 2)
+k.reviews.create(content: "Review Number 12", user_id: 3, stars: 3)
 
-l.reviews.create(content: "Review Number 13", user_id: 4)
-l.reviews.create(content: "Review Number 14", user_id: 3)
+l.reviews.create(content: "Review Number 13", user_id: 4, stars: 2)
+l.reviews.create(content: "Review Number 14", user_id: 3, stars: 1)
 
-m.reviews.create(content: "Review Number 15", user_id: 2)
-m.reviews.create(content: "Review Number 16", user_id: 1)
+m.reviews.create(content: "Review Number 15", user_id: 2, stars: 5)
+m.reviews.create(content: "Review Number 16", user_id: 1, stars: 4)
  
-n.reviews.create(content: "Review Number 17", user_id: 2)
-n.reviews.create(content: "Review Number 18", user_id: 4)
+n.reviews.create(content: "Review Number 17", user_id: 2, stars: 3)
+n.reviews.create(content: "Review Number 18", user_id: 4, stars: 5)
 
-o.reviews.create(content: "Review Number 19", user_id: 3)
-o.reviews.create(content: "Review Number 20", user_id: 1)
+o.reviews.create(content: "Review Number 19", user_id: 3, stars: 2)
+o.reviews.create(content: "Review Number 20", user_id: 1, stars: 1)
